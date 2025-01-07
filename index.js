e27ceb6df01c0b7e5082a342e68ec56cc4f0f4ac
Sultana Yeasmin <EO005092@bifpcl.net>
2025-01-07 21:57:07 +0600

    add-query-to-db-partadded

--- index.js
+++ index.js
@@ -28,9 +28,30 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // Get the database and collection on which to run the operation
+    const database = client.db("syAneDB");
+    const queryCollection = database.collection("queries");
+    const recommendationCollection = database.collection("recommendations");
+    
+    //post a query
+    app.post('/add-query', async(req, res)=>{
+      const queryData = req.body;
+      const result = await queryCollection.insertOne(queryData);
+      console.log(result)
+      res.send(result)
+    })
+
+   
+
+
+
+
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
